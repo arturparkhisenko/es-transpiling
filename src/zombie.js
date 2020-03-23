96e72bd4544107e1455940871fa94a6656968a27
arturparkhisenko <ikeagold@gmail.com>
2020-03-23 19:53:55 -0400

    add universal example

---
+++ src/zombie.js
@@ -0,0 +1,17 @@
+function getRandomQuote() {
+  return Math.random() < 0.5 === true ? 'No guts, no gory.' : 'Me eat brains!';
+}
+
+export { getRandomQuote };
+
+export function generateName(name = 'zGhoul') {
+  return 'z' + name;
+}
+
+export default function zombieFactory() {
+  return {
+    name: generateName(),
+    damage: 42,
+    say: getRandomQuote
+  };
+}
